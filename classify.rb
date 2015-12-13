f0e17f32fc4ce110621bd8e3db109c119884436a
Dunya Kirkali <dunyakirkali@gmail.com>
2015-12-13 17:52:38 +0100

    Add classify

---
+++ classify.rb
@@ -0,0 +1,20 @@
+require 'rmagick'
+require 'fileutils'
+
+Dir.foreach('classify') do |item|
+  next if item == '.' or item == '..' or item == '.DS_Store'
+
+  img =  Magick::Image.read('classify/' + item).first
+  # gscale = img.quantize(256)
+  pix = img.scale(1, 1)
+  avg = pix.pixel_color(0,0)
+  avg_color = avg
+  val = (avg_color.red / 257 + avg_color.green / 257 + avg_color.blue / 257) / 3
+  random_name = (0...5).map { ('a'..'z').to_a[rand(26)] }.join
+
+  puts "#{item} => #{val.to_s.rjust(3, '0')}/#{random_name}.jpg"
+
+  scale_image = img.scale(16, 16)
+
+  scale_image.write('samples/' + val.to_s.rjust(3, '0') + '/' + random_name + '.jpg')
+end
