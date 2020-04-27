1355f8e67d4b2d7e18e6740fff16fbe227bd804c
Dunya Kirkali <dunyakirkali@dMac.home>
2020-04-27 14:54:55 +0200

    Grayscale it

--- classify.rb
+++ classify.rb
@@ -5,8 +5,8 @@ Dir.foreach('classify') do |item|
   next if item == '.' or item == '..' or item == '.DS_Store'
 
   img =  Magick::Image.read('classify/' + item).first
-  # gscale = img.quantize(256)
-  pix = img.scale(1, 1)
+  gscale = img.quantize(256, Magick::GRAYColorspace)
+  pix = gscale.scale(1, 1)
   avg = pix.pixel_color(0,0)
   avg_color = avg
   val = (avg_color.red / 257 + avg_color.green / 257 + avg_color.blue / 257) / 3
@@ -14,7 +14,7 @@ Dir.foreach('classify') do |item|
 
   puts "#{item} => #{val.to_s.rjust(3, '0')}/#{random_name}.jpg"
 
-  scale_image = img.scale(16, 16)
+  scale_image = gscale.scale(16, 16)
 
   scale_image.write('samples/' + val.to_s.rjust(3, '0') + '/' + random_name + '.jpg')
 end
